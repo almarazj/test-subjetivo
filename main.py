7db3f6e38feac236ba94f7bb4a55a6dee15bdfbe
almarazj <almaraz43493@estudiantes.untref.edu.ar>
2024-10-08 13:43:11 -0300

    Put all comparisons in a single form

--- main.py
+++ main.py
@@ -16,10 +16,9 @@ total_comparaciones = 16
 # Configuración básica
 st.set_page_config(page_title="Test Subjetivo", layout="centered")
 # Inicialización de session_state
-if "comparacion_actual" not in st.session_state:
-    st.session_state["comparacion_actual"] = 0
+if "pagina_actual" not in st.session_state:
+    st.session_state["pagina_actual"] = "registro"
     st.session_state["resultados"] = [None] * total_comparaciones
-    st.session_state["disabled"] = True
     
     # Generar una lista con las comparaciones a realizar en orden aleatorio
     audio_folder = Path("data/audio_samples/")
@@ -57,7 +56,7 @@ def play_audio(audio_path):
 st.title("Test de Calidad de Audio - DMOS")
 
 # Datos del sujeto
-if st.session_state["comparacion_actual"] == 0:
+if st.session_state["pagina_actual"] == "registro":
     with st.form(key="datos_sujeto_form"):
         age = st.number_input("Edad:", min_value=0, max_value=120)
         exp = st.selectbox("Experiencia de escucha:", ["Escucho música regularmente", "Trabajo en algo relacionado con la música", "No suelo escuchar música"])
@@ -68,28 +67,27 @@ if st.session_state["comparacion_actual"] == 0:
             st.session_state.age = age
             st.session_state.exp = exp
             st.session_state.gender = gender
-            st.session_state["comparacion_actual"] += 1
+            st.session_state["pagina_actual"] = "comparaciones"
             st.rerun()
             
 # Si el usuario está en la primera comparación o en cualquier comparación posterior
-if st.session_state["comparacion_actual"] > 0:
-    comparacion_actual = st.session_state["comparacion_actual"]
+if st.session_state["pagina_actual"] == "comparaciones":
 
-    if comparacion_actual <= total_comparaciones:
-        # Seleccionamos un audio y un sistema aleatorio
-        audio_a, audio_b = st.session_state["comparaciones"][comparacion_actual - 1]
+    # Iniciamos el formulario de comparación
+    with st.form(key="dmos_form"):
+        
+        for i in range(total_comparaciones):
 
-        # Iniciamos el formulario de comparación
-        with st.form(key="dmos_form"):
-            st.write(f"Comparación {comparacion_actual} de {total_comparaciones}")
+            st.write(f"Comparación {i+1} de {total_comparaciones}")
+            
+            audio_a, audio_b = st.session_state["comparaciones"][i]
             
             st.write("Audio A")
             play_audio(audio_a)
 
             st.write("Audio B")
             play_audio(audio_b)
-            
-            st.write(f"¿Cómo describirías la degradación percibida en el audio B con respecto al audio A?")
+        
             # Opciones de puntaje DMOS con radio buttons
             opciones = {
                 5: "Degradación inaudible",
@@ -100,49 +98,42 @@ if st.session_state["comparacion_actual"] > 0:
             }
 
             # Actualización del puntaje de la comparación
-            st.session_state["resultados"][comparacion_actual - 1] = st.radio(
-                "Calidad percibida", 
+            st.session_state["resultados"][i] = st.radio(
+                "¿Cómo describirías la degradación percibida en el audio B con respecto al audio A?", 
                 list(opciones.keys()), 
                 format_func=lambda x: opciones[x], 
-                key=f"dmos_{comparacion_actual}",
-                index=st.session_state["resultados"][comparacion_actual - 1]
+                key=f"dmos_{i}",
+                index=st.session_state["resultados"][i]
             )
-            
-            # Botones "Volver atrás" y "Siguiente" o "Finalizar" en la última comparación
-            col1, col2 = st.columns(2)
-            with col1:
-                submitted = st.form_submit_button("Atrás")
-                if submitted:
-                    st.session_state["comparacion_actual"] -= 1
-                    st.rerun()
-            with col2:
-                if comparacion_actual <= total_comparaciones-1:
-                    submitted = st.form_submit_button("Siguiente")
-                    if submitted:
-                        st.session_state["comparacion_actual"] += 1
-                        st.rerun()
-                else:
-                    submitted = st.form_submit_button("Finalizar")
-                    if submitted:
-                        nuevo_documento = {
-                            "id_participante": ''.join(random.choices(string.ascii_letters + string.digits, k=4)),   # ID generado
-                            "edad": st.session_state.age,                   # Edad
-                            "genero": st.session_state.gender,              # Género
-                            "experiencia": st.session_state.exp,            # Experiencia de escucha
-                            "resultados": st.session_state.resultados       # Resultados
-                        } 
-                        collection.insert_one(nuevo_documento)
-                        st.success("Resultados guardados correctamente en la base de datos.")
-                        # Avanzamos a la pantalla de agradecimiento
-                        st.session_state["comparacion_actual"] += 1
-                        st.rerun()
-                        
-    else:
-        st.success("Resultados guardados correctamente en la base de datos.")
-        st.write("¡Prueba finalizada! Gracias por participar.")
-        # Limpiar el estado para una nueva sesión (opcional)
-        submitted = st.button("Comenzar una nueva prueba")
-        if submitted:
-            st.session_state["comparacion_actual"] = 0
-            st.session_state["resultados"] = [None] * total_comparaciones
-            st.rerun()
+        
+        # Botones "Volver atrás" y "Siguiente" o "Finalizar" en la última comparación
+        col1, col2 = st.columns(2)
+        with col1:
+            submitted = st.form_submit_button("Atrás")
+            if submitted:
+                st.session_state["pagina_actual"] = "registro"
+                st.rerun()
+        with col2:
+            submitted = st.form_submit_button("Finalizar")
+            if submitted:
+                nuevo_documento = {
+                    "id_participante": ''.join(random.choices(string.ascii_letters + string.digits, k=4)),   # ID generado
+                    "edad": st.session_state.age,                   # Edad
+                    "genero": st.session_state.gender,              # Género
+                    "experiencia": st.session_state.exp,            # Experiencia de escucha
+                    "resultados": st.session_state.resultados       # Resultados
+                } 
+                collection.insert_one(nuevo_documento)
+                # Avanzamos a la pantalla de agradecimiento
+                st.session_state["pagina_actual"] = "final"
+                st.rerun()
+             
+if st.session_state["pagina_actual"] == "final":
+    st.success("Resultados guardados correctamente en la base de datos.")
+    st.write("¡Prueba finalizada! Gracias por participar.")
+    # Limpiar el estado para una nueva sesión (opcional)
+    submitted = st.button("Comenzar una nueva prueba")
+    if submitted:
+        st.session_state["pagina_actual"] = "registro"
+        st.session_state["resultados"] = [None] * total_comparaciones
+        st.rerun()
